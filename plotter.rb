18b188a9e08895fe2f09e9ae36ceb0bb2d21b922
rhapsodhy <me@rhapsodhy.hu>
2010-11-26 00:45:09 +0100

    moved widget to separate file; added MIT licence

--- plotter.rb
+++ plotter.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Widget for plotting
+# Plotting application
 #
 # Copyright (c) 2010 Peter Parkanyi <me@rhapsodhy.hu>.
 #
@@ -22,37 +22,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-require 'rubygems'
-require 'Qt4'
+require 'rgbplot'
 
+app = Qt::Application.new(ARGV)
+window = Qt::MainWindow.new do |w|
+  widget = RGBPlot.new -10, 45
 
-class RGBPlot < Qt::Widget
-  def paintEvent(e)
-    x = @from
-    fstep = (@to - @from) / width.to_f
+  widget.red   = lambda { |x| 128 + 128 * Math.sin(x + 10) }
+  widget.green = lambda { |x| 128 + 128 * Math.sin((x + 10)/10) }
+  widget.blue  = lambda { |x| 128 + 128 * Math.sin((x + 10)/2) }
 
-    painter = Qt::Painter.new self
-
-    (0...width).each do |i|
-      painter.pen = Qt::Pen.new { |p|
-        p.width = 1;
-        p.color = Qt::Color.new red[x], green[x], blue[x]
-      }
-      painter.drawLine i, 0, i, height - 1
-
-      x += fstep
-    end
-
-    painter.end
-  end
-
-  def initialize(from, to, parent = nil)
-    super parent
-
-    @from = from
-    @to   = to
-  end
-
-  attr_accessor :red, :green, :blue
+  w.centralWidget = widget
 end
 
+window.show
+app.exec
