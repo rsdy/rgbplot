ca0298d77570aac8b4b891e14334058d1a619d59
rhapsodhy <me@rhapsodhy.hu>
2010-11-25 22:54:13 +0100

    attr_accessors for the functions of the channels

--- rgbplot.rb
+++ rgbplot.rb
@@ -4,36 +4,44 @@ require 'rubygems'
 require 'Qt4'
 
 
-class Widget < Qt::Widget
-  Start = -10
-  End = 45
-
-  def rf(x); 128 + 128 * Math.sin(x + 10); end
-  def gf(x); 128 + 128 * Math.sin((x + 10)/10); end
-  def bf(x); 128 + 128 * Math.sin((x + 10)/2); end
-
+class RGBPlot < Qt::Widget
   def paintEvent(e)
-    x = Start
-    fstep = (End - Start) / width.to_f
+    x = @from
+    fstep = (@to - @from) / width.to_f
 
-    puts fstep
     painter = Qt::Painter.new self
 
     (0...width).each do |i|
       painter.pen = Qt::Pen.new { |p|
         p.width = 1;
-        p.color = Qt::Color.new rf(x), gf(x), bf(x)
+        p.color = Qt::Color.new red[x], green[x], blue[x]
       }
       painter.drawLine i, 0, i, height - 1
 
       x += fstep
     end
   end
+
+  def initialize(from, to, parent = nil)
+    super parent
+
+    @from = from
+    @to   = to
+  end
+
+  attr_accessor :red, :green, :blue
 end
 
 app = Qt::Application.new(ARGV)
-window = Qt::MainWindow.new
-window.centralWidget = Widget.new
+window = Qt::MainWindow.new do |w|
+  widget = RGBPlot.new -10, 45
+
+  widget.red   = lambda { |x| 128 + 128 * Math.sin(x + 10) }
+  widget.green = lambda { |x| 128 + 128 * Math.sin((x + 10)/10) }
+  widget.blue  = lambda { |x| 128 + 128 * Math.sin((x + 10)/2) }
+
+  w.centralWidget = widget
+end
 
 window.show
 app.exec
